9d6b946e1a3a31fa979ae0c3f8ff5e72f840882f
Simon <10352679+simonhammes@users.noreply.github.com>
2023-06-04 10:53:59 +0000

    gothanks: add 0.5.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -6,6 +6,12 @@ class Gothanks < Formula
   sha256 "932915b470a3c55edf6b6524d0062a762ae79c2c9b39685aa1a53aae53449265"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/simonhammes/homebrew-test-tap-new/releases/download/gothanks-0.5.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "b691cd1bb57148cc6299c21f6ab5fe0c51080665866418428dc531dd5cf08b23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6dc7e1209cb0ef71d42e65bc87e09d4610e9f6525a7ae44bdb13341bb8468813"
+  end
+
   depends_on "go" => :build
 
   def install
